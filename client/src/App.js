b057d2584e6eec22a4aea8e1dedb41c3a7223442
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-05-04 10:57:44 -0400

    set up sign in on client side

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import { Route, Routes, useNavigate } from "react-router-dom";
+// import { useState, useEffect, createContext } from "react";
+
+export function App() {
+  const handleAuth = () => {};
+
+  return (
+    <div>
+      <button onClick={handleAuth}>Sign in with Google</button>
+    </div>
+  );
+}
