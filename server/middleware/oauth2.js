bc04400c933c8202cea2e9ad5aefc7ae8763c7ea
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-05-05 10:06:38 -0400

    revert to previous commit

--- server/middleware/oauth2.js
+++ server/middleware/oauth2.js
@@ -10,78 +10,61 @@ const keys = require("../keys.json");
 // acquire pre autheneticated oauth2 client
 async function main() {
   // make request to people api
-  getAuthenticatedClient();
+  const oAuth2Client = await getAuthenticatedClient();
   const url = "https://people.googleapis.com/v1/people/me?personFields=names";
-  // const client = createServer();
-  // const res = await client.request({ url });
-  // console.log(res.data);
+  const res = await oAuth2Client.request({ url });
+  console.log(res.data);
 
-  // // check access token info when acquired
-  // const tokenInfo = await oAuth2Client.getTokenInfo(
-  //   oAuth2Client.credentials.access_token
-  // );
-  // console.log(tokenInfo);
+  // check access token info when acquired
+  const tokenInfo = await oAuth2Client.getTokenInfo(
+    oAuth2Client.credentials.access_token
+  );
+  console.log(tokenInfo);
 }
-
-let oAuth2Client;
-let authorizeUrl;
 
 // create a new oauth2 client, returns full client to cb
 function getAuthenticatedClient() {
-  console.log("in authenticate client func");
+  return new Promise((resolve, reject) => {
+    const oAuth2Client = new OAuth2Client(
+      keys.web.client_id,
+      keys.web.client_secret,
+      keys.web.redirect_uris[0]
+    );
 
-  oAuth2Client = new OAuth2Client(
-    keys.web.client_id,
-    keys.web.client_secret,
-    keys.web.redirect_uris[0]
-  );
-  console.log(oAuth2Client);
-
-  // generates url for consent dialogue
-  authorizeUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: "https://www.googleapis.com/auth/userinfo.profile",
-    prompt: "consent",
-  });
-  console.log(authorizeUrl);
-}
-
-// destroyer(server);
-
-function createServer(req, res, next) {
-  // open an http server to accept oauth callback
-  console.log("in crate server outer");
-  const server = http
-    .createServer(async (req, res) => {
-      console.log(req.body);
-      try {
-        if (req.url.indexOf("/auth/redirect") > -1) {
-          console.log("in url indexOf");
-          const qs = new url.URL(req.url, "https://localhost:6000")
-            .searchParams;
-          const code = qs.get("code");
-          console.log(`Code is: ${code}`);
-          res.end("Auth successful! Please return to console");
-          server.destroy();
-
-          // use code to acquire tokens
-          const r = await oAuth2Client.getToken(code);
-          oAuth2Client.setCredentials(r.tokens);
-          console.info("Tokens acquired.");
-          return oAuth2Client;
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    })
-    .listen(6000, () => {
-      open(authorizeUrl, { wait: false }).then((cp) => cp.unref());
+    // generates url for consent dialogue
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: "https://www.googleapis.com/auth/userinfo.profile",
+      prompt: "consent",
     });
+
+    // open an http server to accept oauth callback
+    const server = http
+      .createServer(async (req, res) => {
+        try {
+          if (req.url.indexOf("google/auth/callback") > -1) {
+            const qs = new url.URL(req.url, "https://localhost:6000")
+              .searchParams;
+            const code = qs.get("code");
+            console.log(`Code is: ${code}`);
+            res.end("Auth successful! Please return to console");
+            server.destroy();
+
+            // use code to acquire tokens
+            const r = await oAuth2Client.getToken(code);
+            oAuth2Client.setCredentials(r.tokens);
+            console.info("Tokens acquired.");
+            resolve(oAuth2Client);
+          }
+        } catch (e) {
+          reject(e);
+        }
+      })
+      .listen(6000, () => {
+        open(authorizeUrl, { wait: false }).then((cp) => cp.unref());
+      });
+    destroyer(server);
+  });
 }
 
-// main().catch(console.error);
-
-module.exports = {
-  main,
-  createServer,
-};
+module.exports = main;
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const { main, createServer } = require("../middleware/oauth2");
+const main = require("../middleware/oauth2");
 
 router.post("/login", (req, res, next) => {
+  console.log("in login route");
   main();
 });
 
 router.get("/redirect", (req, res, next) => {
   console.log("in callback");
-  createServer();
+  console.log(res.body);
+  // resolve something here that triggers the server in oauth func
+  console.log(req.body);
 });
 
 module.exports = router;
